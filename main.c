1959d09a539ca57f95a5f28fbe0c5d472b31a861
minjiwkim <minji5489235790@gmail.com>
2023-10-12 17:35:36 +0900

    5

--- main.c
+++ main.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int square(int a)
+int factorial(int n)
 {
-    return (a*a);
+    int i;
+    int res = 1;
+    for (i=0; i<n; i++)
+    {
+        res=res*(i+1);
+    }
+    return res;
 }
 
-int main()
+int combination (int n, int r)
 {
-    int a=2;
-    a = square(a);
-    printf("a=%i\n", a);
+    int div1, div2; //div1 - 분자, div2 - 분모
     
-    system("PAUSE");
+    div1 = factorial(n); //n!
+    div2 = factorial(n-r) * factorial(r);
+    
+    // 분모, 분자 나누기 연산 
+    return (div1/div2);
+} 
+
+int main(int argc, char *argr[])
+{
+    int n, r; 
+    int result;
+    
+    //1. 입력값 받음 
+      // 메시지 출력 
+    printf("input n : ");
+      // n 입력 
+    scanf("%d", &n);
+      // 메시지 출력
+    printf("input r : ");
+      // r 입력
+    scanf("%d", &r);
+    
+    //2. 분모/분자 계산
+    result = combination(n, r);
+    
+    //3. 최종 값 출력 
+      // 결과 출력 
+    printf("result is %i\n", result);
+
+system("PAUSE");
 }
